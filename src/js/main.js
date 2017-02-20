c21fe29e4b259444b26e4f4d77cb9b941d719e90
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-19 19:48:31 -0500

    added music and changed format

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,91 @@
 import $ from 'jquery';
 import token from "./token.js"
 
-function test(data) {
-  $.ajax({
-    url: `http://api.soundcloud.com/tracks/?client_id=${token}`,
-    dataType: "json",
-    data: {
-    q: data
-  },
-  success: console.log
-  });
+
+
+function getData(data, callback) {
+    $.ajax({
+        url: `http://api.soundcloud.com/tracks/?client_id=${token}`,
+        dataType: "json",
+        data: {
+            q: data
+        },
+        success: callback
+    })
 };
 
 
-function playMusic{
+
+
+$(".music-player").html(`
+
+<audio class="playthesong" src=""controls></audio>
+<div class="nowplay"><p>Now Playing:</p></div>
+
+`)
+
+
+//https://api.soundcloud.com/tracks/308225130/stream?client_id=${token}
+
+
+function appendData(songs) {
+    console.log(songs)
+    for (var count = 0; count < songs.length; count++) {
+        $(".music").append(
+            `
+
+    <div class="musicStuff">
+
+      <img class="song-pictures" data-id="${songs[count].id}" src="${songs[count].artwork_url}"/>
+      <div class="musicStream">${songs[count].stream_url}?client_id=${token}</div>
+    <ul>
+      <li><div class="titles">${songs[count].title}</div></li>
+    </ul>
+
+    </div>
+    `)
+        $(".musicStream").hide();
+    }
+
+
+    $(".song-pictures").on("click", function pickSong(event) {
+        var target = $(event.currentTarget).next(".musicStream").html();
+        playSong(target);
+    });
+
+    $(".song-pictures").on("click", function nowPlaying(event) {
+        var othertarget = $(event.currentTarget).next(".titles").html();
+        putSong(othertarget);
+
+    });
+
+
+}
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+function playSong(target) {
+    $(".playthesong").replaceWith(`<audio src="${target}" controls></audio>`)
+
+};
+
+
+
+function putSong(othertarget) {
+    $(".nowplay").replaceWith(`<p>Now Playing:${othertarget}</p>`)
+}
+
+
+
+
+
+function searchMusic(event) {
+    event.preventDefault();
+    $(".music").empty();
+    var input = $(".input").val();
+    getData(input, appendData);
 
 }
 
-$(".container").html(`
-  <audio src="https://api.soundcloud.com/tracks/259021543/stream?client_id=${token}"controls></audio>`)
-
-test("thewillpeterson");
-
-
-//1. Function that listens for button
-//2. onclick buttton- runs function that loops through the music. plays music
-//3. another function that displays all the music data on the page.
+$(".Searchformusic").click(searchMusic)
